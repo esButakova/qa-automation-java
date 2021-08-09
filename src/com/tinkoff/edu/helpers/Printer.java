c16f08bb34c875f02c8d3bdcff77ae6d54a66bbe
esButakova <e.butakova@tinkoff.ru>
2021-08-09 15:20:19 +0300

    Задание 2 - Приложение скомпилировано, исходники отделены от бинарников

---
+++ src/com/tinkoff/edu/helpers/Printer.java
@@ -0,0 +1,12 @@
+package com.tinkoff.edu.helpers;
+
+/**
+ * Created on 08.08.2021
+ *
+ * @author Elena Butakova
+ */
+public class Printer {
+    public static void print(String message) {
+        System.out.println(message);
+        }
+}
