cbf094158dbb23e7250b0e156b6ddde7081992e8
esButakova <e.butakova@tinkoff.ru>
2021-08-10 23:02:46 +0300

    Задание 3 - Сгенерирован maven-проект, используется тестовая зависимость JUnit5, проект собирается и запускается.

---
+++ src/test/java/com/tinkoff/edu/AppTest.java
@@ -0,0 +1,22 @@
+package com.tinkoff.edu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created on 10.08.2021
+ *
+ * @author Elena Butakova
+ *
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
